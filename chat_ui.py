7c4009eb64953caeb36cb0607c0e62aa0802942e
bg-write <bradywgerber@gmail.com>
2023-04-19 12:08:20 -0400

    refactor chat_ui message

--- chat_ui.py
+++ chat_ui.py
@@ -15,16 +15,23 @@ messages = [{
     "role":
     "system",
     "content":
-    "You are a music curator who helps people discover great new music from any era or genre."
+    "You are a music curator with decades of experience in the music industry who helps people discover great new music from any era or genre. You value the classics yet always try to steer new listeners to discover music they might not have heard before."
 }]
 
 
 def custom_chat(user_input):
     '''Prompts user input, sends to OpenAI, prints response'''
     messages.append({"role": "user", "content": user_input})
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=messages)
-    chat_reply = response["choices"][0]["message"]["content"]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.7,
+        max_tokens=1000,
+        n=1,
+        stop=None,
+        timeout=10,
+    )
+    chat_reply = response.choices[0].message.content
     messages.append({"role": "assistant", "content": chat_reply})
     return chat_reply
 
@@ -35,3 +42,5 @@ demo = gr.Interface(fn=custom_chat,
                     title="Discover Your New Favorite Songs!")
 
 demo.launch(share=True)
+
+# click "control + c" to exit out of the chat and restart
